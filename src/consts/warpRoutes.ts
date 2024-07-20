8191ccb1e00511f99cb8a4f7f44e46d057b25258
kamal eth <kamalthedev7@gmail.com>
2024-07-20 17:22:56 +0530

    add the warp route

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -4,13 +4,35 @@ import { TokenStandard } from '@hyperlane-xyz/sdk';
 // These configs will be merged with the warp routes in the configured registry
 // The input here is typically the output of the Hyperlane CLI warp deploy command
 export const warpRouteConfigs: WarpCoreConfig = {
-  tokens: [{
-    symbol: "DUSDC",
-    name: "DChain USDC",
-    decimals: 18,
-    chainName: "dchaintestnet",
-    standard: TokenStandard.ERC20,
-    addressOrDenom: "0x2AAC535db31DB35D13AECe36Ea7954A2089D55bE"
-  }],
+  tokens: [
+    {
+      // The ChainName of the token
+      chainName: 'sepolia',
+      // See https://github.com/hyperlane-xyz/hyperlane-monorepo/blob/main/typescript/sdk/src/token/TokenStandard.ts
+      standard: TokenStandard.EvmHypCollateral,
+      // The token metadata (decimals, symbol, name)
+      decimals: 6,
+      symbol: 'USDC',
+      name: 'USD Coin',
+      // The router address
+      addressOrDenom: '0xF6fe2F280b67E0177fac2893Cdc80e95578448da',
+      // The address of the underlying collateral token
+      collateralAddressOrDenom: '0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8',
+      // A path to a token logo image
+      logoURI: '/logos/usdc.png',
+      // The list of tokens this one is connected to
+      connections: [ { token: 'ethereum|dchaintestnet|0x76414c98ee9AD3F776054f16A351831b71870Ff3' } ]
+    },
+    {
+      chainName: 'dchaintestnet',
+      standard: TokenStandard.EvmHypSynthetic,
+      decimals: 6,
+      symbol: 'USDC',
+      name: 'USD Coin',
+      addressOrDenom: '0x76414c98ee9AD3F776054f16A351831b71870Ff3',
+      logoURI: '/logos/usdc.png',
+      connections: [ { token: 'ethereum|sepolia|0xF6fe2F280b67E0177fac2893Cdc80e95578448da' } ]
+    }
+  ],
   options: {},
 };
