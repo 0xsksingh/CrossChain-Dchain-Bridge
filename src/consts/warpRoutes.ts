f7f588f8a91cb6675b47eb19b8d1e4b39825a7bc
kamal eth <kamalthedev7@gmail.com>
2024-07-20 13:17:55 +0530

    image added dchain

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -1,9 +1,16 @@
 import { WarpCoreConfig } from '@hyperlane-xyz/sdk';
-
+import { TokenStandard } from '@hyperlane-xyz/sdk';
 // A list of Warp Route token configs
 // These configs will be merged with the warp routes in the configured registry
 // The input here is typically the output of the Hyperlane CLI warp deploy command
 export const warpRouteConfigs: WarpCoreConfig = {
-  tokens: [],
+  tokens: [{
+    symbol: "DUSDC",
+    name: "DChain USDC",
+    decimals: 18,
+    chainName: "dchaintestnet",
+    standard: TokenStandard.ERC20,
+    addressOrDenom: "0x2AAC535db31DB35D13AECe36Ea7954A2089D55bE"
+  }],
   options: {},
 };
